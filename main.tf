438a21c263ff9e3c3a5dc7558079088318bfe52f
Hojoong Jang <hojoong310@gmail.com>
2025-01-16 00:39:05 +0900

    feat: rds

--- main.tf
+++ main.tf
@@ -94,10 +94,50 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 }
 
 # RDS Instance
+resource "aws_db_instance" "rds_instance" {
+  identifier           = "rds-instance"
+  allocated_storage    = 200
+  engine               = "postgres"
+  engine_version       = "16.3"
+  instance_class       = "db.t4g.micro"
+  username             = "Airflow"
+  password             = "Airflow123!"
+  parameter_group_name = "default.postgres16"
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
 
+  publicly_accessible = false
+  skip_final_snapshot = true
 
+  tags = {
+    Name = "Airflow-MetaDB"
+  }
+}
 
+# RDS Security Group
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] # VPC CIDR 블록
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "RDS-Security-Group"
+  }
+}
 
 # ElastiCache Private Subnets
 resource "aws_subnet" "private_elasticache_subnets" {
@@ -138,7 +178,6 @@ resource "aws_elasticache_cluster" "elasticache_cluster" {
 # ElastiCache Security Group
 resource "aws_security_group" "elasticache_sg" {
   name        = "elasticache-sg"
-  description = "Allow Redis access to ElastiCache"
   vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
